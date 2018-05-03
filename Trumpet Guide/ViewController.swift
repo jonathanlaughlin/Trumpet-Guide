4da28afa55a84a8bf1a8f5caf648e22571842db8
Jonathan Laughlin <jlaughli@calvin.edu>
2018-05-03 13:37:27 -0400

    Initial Commit

---
+++ Trumpet Guide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trumpet Guide
+//
+//  Created by jlaughli on 5/3/18.
+//  Copyright © 2018 Jonathan Laughlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
